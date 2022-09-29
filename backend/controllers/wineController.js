e477ba1f6c1c13179c92c5468f08cbe7bbb928d6
JustinM099 <justinm099@mac.com>
2022-09-29 14:26:04 -0700

    error middleware & async handling

--- backend/controllers/wineController.js
+++ backend/controllers/wineController.js
@@ -1,39 +1,48 @@
+const asyncHandler = require('express-async-handler')
 
 //desc: get all wines
 //route: GET api/wines
 //access: private
-const getWines = (req, res) => {
-    res.status(200).json({message: 'Get wines'})
-}
+const getWines = asyncHandler(async (req, res) => {
+
+        res.status(200).json({ message: 'Get wines' })
+        
+})
 
 //desc: add wine
 //route: POST api/wines
 //access: private
-const addWine = (req, res) => {
+const addWine =  asyncHandler(async (req, res) => {
+
+    if (!req.body.text) { 
+        res.status(400)
+        throw new Error('no text field')
+    }
+
     console.log(req.body)
     res.status(200).json({message: 'Add wine'})
-}
+})
 
 //desc: get one wine
 //route: GET api/wines/:id
 //access: private
-const getWine = (req, res) => {
+const getWine = asyncHandler(async (req, res) => {
     res.status(200).json({message: `Get wine ${req.params.id}`})
-}
+})
 
 //desc: edit wine
 //route: PUT api/wines/:id
 //access: private
-const editWine = (req, res) => {
+const editWine = asyncHandler(async (req, res) => {
     res.status(200).json({message: `Update wine ${req.params.id}`})
-}
+})
 
 //desc: delete wine
 //route: DELETE api/wines/:id
 //access: private
-const deleteWine = (req, res) => {
+const deleteWine = asyncHandler(async (req, res) => {
     res.status(200).json({message: `Delete wine ${req.params.id}`})
-}
+})
 
 module.exports = {
     getWines,
--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const dotenv = require('dotenv').config()
+const { errorHandler } = require('./middleware/errorMiddleware')
 const PORT = process.env.PORT || 5000
 
 const app = express()
@@ -8,5 +9,7 @@ app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 
 app.use('/api/wines', require('./routes/wineRoutes'))
+
+app.use(errorHandler)
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
