9537143a1637331cbef1a5db5cced59fee35d8a9
JustinM099 <justinm099@mac.com>
2022-11-01 23:54:22 -0700

    working on password reset with nodemailer

---
+++ backend/routes/mailRoutes.js
@@ -0,0 +1,32 @@
+const User = require('../models/userModel')
+const express = require('express')
+const nodemailer = require('nodemailer')
+const router = express.Router()
+
+router.post('/', (req, res) => {
+    const output = `
+    <h1>thank you for choosing bottlesDb</h1>
+    <h3>your account details are below:</h3>
+    <ul>
+        <li>username: ${req.body.name}</li>
+        <li>email: ${req.body.email}</li>
+    </ul>
+    <p>bottlesDb</p>
+    `
+})
+
+//create reusable transporter object using the default SMTP transport
+const main = async () => {
+
+    let testAccount = await nodemailer.createTestAccount()
+
+    const transporter = nodemailer.createTransport({
+        host: 'smtp.gmail.com',
+        port: 587,
+        secure: false, //change later, change port to 465
+        auth: {
+            user: testAccount.user,
+            pass: testAccount.pass
+        }
+    })
+}
